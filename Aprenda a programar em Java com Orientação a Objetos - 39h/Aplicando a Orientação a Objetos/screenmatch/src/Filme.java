5fab5ef3c12084a64fd0ceef3ea8c48ad28d5602
Sergio Pelegrini <serpel@gmail.com>
2024-01-07 09:51:11 -0300

    Encapsulamento - OO

--- Aprenda a programar em Java com Orientação a Objetos - 39h/Aplicando a Orientação a Objetos/screenmatch/src/Filme.java
+++ Aprenda a programar em Java com Orientação a Objetos - 39h/Aplicando a Orientação a Objetos/screenmatch/src/Filme.java
@@ -2,7 +2,25 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
-    int totalDeAvaliacoes;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota) { // argumento nota do tipo double
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 }
