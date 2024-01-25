f01e04987173fa5242703d1b2904575793ca9a2c
Sergio Pelegrini <serpel@gmail.com>
2024-01-25 16:36:33 -0300

    exceções, arquivos

--- Aprenda a programar em Java com Orientação a Objetos - 39h/Java consumindo API, gravando arquivos e lidando com erros/screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ Aprenda a programar em Java com Orientação a Objetos - 39h/Java consumindo API, gravando arquivos e lidando com erros/screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -7,6 +7,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -17,6 +18,7 @@ import java.util.Scanner;
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner leitura = new Scanner(System.in);
+
         System.out.println("Digite um filme para busca: ");
         // Como já foi visto anteriormente, o var faz uma inferência do tipo da variável,
         // eliminando assim, definir qual é o tipo na linha abaixo. Mas na verdade tanto
@@ -77,6 +79,12 @@ public class PrincipalComBusca {
             Titulo meuTitulo = new Titulo(meuTituloOmdb);
             System.out.println("Título já convertido:");
             System.out.println(meuTitulo);
+
+            //Escrever em arquivo:
+            FileWriter escrita = new FileWriter("filmes.txt");
+            escrita.write(meuTitulo.toString());
+            escrita.close();
+
         } catch (NumberFormatException e) {
             System.out.println("Aconteceu um erro: ");
             System.out.println(e.getMessage());
