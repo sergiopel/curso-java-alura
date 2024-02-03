8988f2fe67cce1f9208ef9a71795af90458b52d4
Sergio Pelegrini <serpel@gmail.com>
2024-02-03 10:39:52 -0300

    finalização do desafio e do curso

--- Aprenda a programar em Java com Orientação a Objetos - 39h/Java consumindo API, gravando arquivos e lidando com erros/buscacep/src/com/comsistemas/buscacep/Endereco.java
+++ Aprenda a programar em Java com Orientação a Objetos - 39h/Java consumindo API, gravando arquivos e lidando com erros/buscacep/src/com/comsistemas/buscacep/Endereco.java
@@ -1,5 +1,22 @@
 package com.comsistemas.buscacep;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public record Endereco(String cep, String logradouro, String complemento,
                        String localidade, String uf) {
+        public void salvaJson(Endereco endereco) throws IOException {
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .setPrettyPrinting()
+                    .create();
+            FileWriter writer = new FileWriter(endereco.cep() + ".json");
+            writer.write(gson.toJson(endereco));
+            writer.close();
+
+        }
 }
--- Aprenda a programar em Java com Orientação a Objetos - 39h/Java consumindo API, gravando arquivos e lidando com erros/buscacep/src/com/comsistemas/buscacep/Principal.java
+++ Aprenda a programar em Java com Orientação a Objetos - 39h/Java consumindo API, gravando arquivos e lidando com erros/buscacep/src/com/comsistemas/buscacep/Principal.java
@@ -2,73 +2,31 @@ package com.comsistemas.buscacep;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class Principal {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
-        FileWriter writer = new FileWriter("ceps.json");
+        var cep = "";
 
         ConsultaCep consultaCep = new ConsultaCep();
 
-        try {
-            Endereco address = consultaCep.buscaEndereco("09750000");
-            System.out.println(address);
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-        }
-
-        System.exit(0);
-
-        String cep = "";
-        String endereco = "";
-        String json;
-
-        while (!cep.equalsIgnoreCase("sair")) {
-            System.out.println("##################################################");
-            System.out.println("#");
-            System.out.println("# Entre com o CEP a consultar:");
-            System.out.println("#");
+        while(!cep.equalsIgnoreCase("sair")) {
+            System.out.println("\nEntre com o Cep a consultar:");
             cep = leitura.next();
             if (cep.equalsIgnoreCase("sair")) {
                 break;
             }
-            System.out.println("##################################################");
-            endereco = "https://viacep.com.br/ws/" + cep + "/json/";
-            //endereco = "https://viacep.com.br/ws/09750000/json/";
-            //System.out.println(endereco);
-
-            // Consumir API de Cep
-            // Usar HttpClient, HttpRequest e HttpResponse
             try {
-                HttpClient client = HttpClient.newHttpClient();
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create(endereco))
-                        .build();
-                HttpResponse<String> response = client
-                        .send(request, HttpResponse.BodyHandlers.ofString());
-                if (response.statusCode() == 400) {
-                    throw new CepFormatoInvalidoException("Cep com formato inválido!");
-                }
-
-                System.out.println(response.statusCode());
-                json = response.body();
-
-                System.out.println("Variável json: " + json);
-
-                writer.write(json);
-
-            } catch (CepFormatoInvalidoException e) {
-                System.out.println("Erro: " + e.getMessage());
-            } catch (Exception e) {
-                System.out.println("Erro: " + e.getMessage());
+                Endereco address = consultaCep.buscaEndereco(cep);
+                System.out.println(address);
+                GeraArquivo gera = new GeraArquivo();
+                gera.salvaJson(address);
+            } catch (RuntimeException | IOException e) {
+                System.out.println(e.getMessage());
             }
         }
 
-        writer.close();
+
     }
 }
